4a0f23857d599c729b29a863e903eeffac947ad5
Suskcx01 <sushmakc2052@gmail.com>
2024-04-30 21:40:21 +0200

    doxygen comment

--- Car.py
+++ Car.py
@@ -1,11 +1,36 @@
 class Car:
+    """
+    A class representing a car.
+
+    Attributes:
+        name (str): The name of the car manufacturer.
+        model (str): The model of the car.
+        color (str): The color of the car.
+        year (int): The manufacturing year of the car.
+
+    Methods:
+        display_info(): Display information about the car.
+    """
+
     def __init__(self, name, model, color, year):
+        """
+        Initializes a new Car object.
+
+        Args:
+            name (str): The name of the car manufacturer.
+            model (str): The model of the car.
+            color (str): The color of the car.
+            year (int): The manufacturing year of the car.
+        """
         self.name = name
         self.model = model
         self.color = color
         self.year = year
 
     def display_info(self):
+        """
+        Display information about the car.
+        """
         print(f"Name: {self.name}")
         print(f"Model: {self.model}")
         print(f"Color: {self.color}")
